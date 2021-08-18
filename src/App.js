390ea846e551d638876d289f769d9b9862709a06
Wilson Seabra <wilson.seabra@vetta.digital>
2021-08-17 22:12:31 -0300

    aula 2 início

--- src/App.js
+++ src/App.js
@@ -4,9 +4,14 @@ import Modal from './Modal';
 
 const App = () => {
   const [modal, setModal] = useState(false);
-
+  const [itens, setItens] = useState('teste');
+  function handleClick() {
+    setItens('outro');
+  }
   return (
     <>
+      <p>{itens}</p>
+      <button onClick={handleClick}>adicionar</button>
       <Modal modal={modal} setModal={setModal} />
       <ButtonModal setModal={setModal} />
     </>
