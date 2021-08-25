ad73b6091ceb2f487a994ea26d26c8160eeb89ae
Wilson Seabra <wilson.seabra@vetta.digital>
2021-08-25 20:51:18 -0300

    commit do exercicio incompleto

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
 import React, { useEffect, useState } from 'react';
 
 const App = () => {
-  const [contar, setContar] = useState(0);
+  const [produto, setProduto] = useState([]);
 
   useEffect(() => {
-    document.title = [contar];
+    fetch('https://ranekapi.origamid.dev/json/api/produto/notebook')
+      .then((response) => response.json())
+      .then((result) => {
+        setProduto(result);
+        return console.log(produto);
+      });
     console.log('executou');
-  }, [contar]);
+  }, [produto]);
   return (
-    <div style={{ height: '200vh' }}>
-      <button onClick={() => setContar(contar + 1)}>{contar}</button>
+    <div>
+      <button onClick={(nomeProduto = 'notebook')}>notebook</button>
+      <span> </span>
+      <button>smartphone</button>
     </div>
   );
 };
 
 export default App;
+
+// Quando o usuário clicar em um dos botões, faça um fetch do produto clicado utilizando a api abaixo
+// https://ranekapi.origamid.dev/json/api/produto/notebook
+// https://ranekapi.origamid.dev/json/api/produto/smartphone
+// Mostre o nome e preço na tela (separe essa informação em um componente Produto.js)
+// Defina o produto clicado como uma preferência do usuário no localStorage
+// Quando o usuário entrar no site, se existe um produto no localStorage, faça o fetch do mesmo
